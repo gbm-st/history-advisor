967fceb945be83edeb85f620a3d0f3229e5a1af5
GBM <geralld.bm@culiacan.tecnm.mx>
2022-10-25 12:36:23 -0600

    Terminando con la mayoria de la logica, ya arroja defuzzificacion.

--- src/logica/logica.js
+++ src/logica/logica.js
@@ -3,79 +3,171 @@ var minutosTranscurridos = 0;
 var segundosTranscurridos = 0;
 var minutos = "";
 var segundos = "";
+var valores = [];
+var verdaderos = 0;
+var falsos = 0;
+var hola;
 
 function obtenerRespuestas(form1, form2, form3, form4, form5) {
     var sizes = form1.length;
     var faltoResponder = true;
     
-    for (i=0; i < sizes; i++) {
-        if (form1[i].checked==true) {
-            alert(form1.elements[i].value);
+    for (i=0; i < sizes; i++) 
+    {
+        if (form1[i].checked==true) 
+        {
+            valores[0] = form1.elements[i].value;
             faltoResponder = false;
         }
     }
-    if (faltoResponder) {
+    if (faltoResponder) 
+    {
         alert('Faltó contestar una pregunta. 1')
         return;
     }
     faltoResponder=true;
 
     sizes = form2.length;
-    for (i=0; i < sizes; i++) {
-        if (form2[i].checked==true) {
-            alert(form2.elements[i].value);
+    for (i=0; i < sizes; i++) 
+    {
+        if (form2[i].checked==true) 
+        {
+            valores[1] = form2.elements[i].value;
             faltoResponder = false;
         }
     }
-    if (faltoResponder) {
+    if (faltoResponder) 
+    {
         alert('Faltó contestar una pregunta. 2')
         return;
     }
     faltoResponder=true;
 
     sizes = form3.length;
-    for (i=0; i < sizes; i++) {
-        if (form3[i].checked==true) {
-            alert(form3.elements[i].value);
+    for (i=0; i < sizes; i++) 
+    {
+        if (form3[i].checked==true) 
+        {
+            valores[2] = form3.elements[i].value;
             faltoResponder = false;
         }
     }
-    if (faltoResponder) {
+    if (faltoResponder) 
+    {
         alert('Faltó contestar una pregunta. 3')
         return;
     }
     faltoResponder=true;
 
     sizes = form4.length;
-    for (i=0; i < sizes; i++) {
-        if (form4[i].checked==true) {
-            alert(form4.elements[i].value);
+    for (i=0; i < sizes; i++) 
+    {
+        if (form4[i].checked==true) 
+        {
+            valores[3] = form4.elements[i].value;
             faltoResponder = false;
         }
     }
-    if (faltoResponder) {
+    if (faltoResponder) 
+    {
         alert('Faltó contestar una pregunta. 4')
         return;
     }
     faltoResponder=true;
 
     sizes = form5.length;
-    for (i=0; i < sizes; i++) {
-        if (form5[i].checked==true) {
-            alert(form5.elements[i].value);
+    for (i=0; i < sizes; i++) 
+    {
+        if (form5[i].checked==true) 
+        {
+            valores[4] = form5.elements[i].value;
             faltoResponder = false;
         }
     }
-    if (faltoResponder) {
+    if (faltoResponder) 
+    {
         alert('Faltó contestar una pregunta. 5')
         return;
     }
 
+    for (i = 0; i < valores.length; i++)
+    {
+        if (valores[i] == "Verdadero") 
+        {
+            verdaderos += 1;
+        }
+        else
+        {
+            falsos += 1;
+        }
+        // alert(valores[i])
+    }
+
+    // alert(falsos + " " + minutosTranscurridos + " " + numeroAyudas);
+    
+    var obj = {
+        crisp_input: [falsos, minutosTranscurridos, numeroAyudas],
+        variables_input: [
+            {
+                name: "Errores",
+                setsName: ["Pocos", "Algunos", "Muchos"],
+                sets: [
+                    [0,0,1,2],
+                    [1,2,3,4],
+                    [3,4,5,5]
+                ]
+            },
+            {
+                name: "Tiempo",
+                setsName: ["Bueno", "Decente", "Mucho"],
+                sets: [
+                    [0,0,0,2],
+                    [1,3,3,5],
+                    [4,6,6,6]
+                ]
+            },
+            {
+                name: "Ayudas",
+                setsName: ["Pocas", "Algunas", "Muchas"],
+                sets: [
+                    [0,0,0,2],
+                    [1,3,3,5],
+                    [4,6,6,6]
+                ]
+            }
+        ],
+        variable_output: {
+            name: "Complejidad",
+            setsName: ["Facil", "Medio", "Dificil"],
+            sets: [
+                [0,0,0,3],
+                [2,5,5,8],
+                [7,10,10,10]
+            ]
+        },
+        inferences: [
+            [2,1,0],
+            [2,1,0],
+            [2,1,0]
+        ]
+    };
+
+    hola = obj;
+
+    document.getElementById("hiddenButton").click();
+
+    // Agarrar errores (falsos), tiempo (minutosTranscurridos), ayudas (numeroAyudas) y pasarlos a fuzzy logic.
     //Falta redirigir en js a otra pagina, las ayudas y el cronometro
+    
     //TimeMe.js/
     //howto_js_redirect_webpage
     //how-to-measure-a-time-spent-on-a-page
 };
+
+function mostrarResultado () 
+{
+    alertdocument.getElementById("result")
+}
 
 function reloj(){
     if (minutosTranscurridos.toString().length == 1)
